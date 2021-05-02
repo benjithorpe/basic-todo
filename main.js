f28fdee5c76c89e0132ccc023494f0fd375a45ab
benjithorpe <benjaminthorpe19@gmail.com>
2021-05-02 09:31:57 +0000

    Fix the delete function

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-var todoList = document.querySelectorAll('#todos ul li');
-var todoListParent = document.querySelector('#todos ul');
+const todoListParent = document.querySelector('#todos ul');
 const addBtn = document.querySelector('button');
 var userInput = document.querySelector('input');
 
 addBtn.addEventListener('click', function(){
   // checks if user has enter a value
-  if(userInput.value.trim() !== ""){
-    // create a new list tag
-    var newItem = document.createElement("li");
+  if(userInput.value.trim()){
+    var newListItem = document.createElement("li"); // create a new list element
+    let userText = document.createTextNode(userInput.value);
 
-    // add the user input in the tag
-    newItem.innerText = userInput.value;
-
-    // add the tag to the ul
-    todoListParent.append(newItem);
+    newListItem.appendChild(userText);  // add the user input in the li element
+    todoListParent.appendChild(newListItem);  // add the list tag to the ul element
     userInput.value = "";  // reset the input value
+    addEventToItem();
   }
 });
+
+
+function addEventToItem(){
+  var todoList = document.querySelectorAll('li');
+
+  for(let count = 0; count < todoList.length; count++){
+    todoList[count].addEventListener('click', function(){
+      todoListParent.removeChild(this);
+    });
+  }
+}
