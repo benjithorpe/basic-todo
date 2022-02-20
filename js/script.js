ff372842123a14a5fff4c59c7be703e1819fca6d
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-02-20 13:59:50 +0000

    Items can be deleted from localStorage

--- js/script.js
+++ js/script.js
@@ -5,33 +5,43 @@ const allTodoItems = document.querySelector("[data-todo-list]");
 const itemTemplate = document.querySelector("[data-todo-item]");
 
 // localStorage.clear()
-// Get items from localStorage
-let localTodos = localStorage.getItem("todos");
+let localTodos = localStorage.getItem("todos");  // Get items from localStorage
 localTodos = localTodos ? JSON.parse(localTodos) : [];
 localTodos.forEach(element => allTodoItems.append(buildItem(element)));
+
+const getInput = () => document.querySelector("[data-input-field]");
+const isValidInput = () => getInput().value.trim() ? true : false;
+// Save data to localStorage
+const saveToStorage = () => localStorage.setItem("todos", JSON.stringify(localTodos));
+const deleteItem = (item) => {
+  // Remove the deleted item from localStorage and UI
+  localTodos.splice(localTodos.indexOf(item), 1);
+  localTodos = [...localTodos];  // Update the todo list
+};
 
 function buildItem(element) {
   const todoItem = itemTemplate.content.cloneNode(true);
 
   todoItem.querySelector("span.content").textContent = element;
   const deleteBtn = todoItem.querySelector("button.delete");
+  // Add delete functionality to item created
   deleteBtn.addEventListener('click', () => {
-    deleteBtn.parentElement.remove()
+    deleteBtn.parentElement.remove();
+    deleteItem(element);
+    saveToStorage();
   });
 
   return todoItem;
 }
 
-addItemBtn.addEventListener('click', () => {
-  const userInput = document.querySelector("[data-input-field]");
-  // Do nothing if no data is entered
-  if (!userInput.value.trim()) return;
-
-  allTodoItems.append(buildItem(userInput.value));
+function addItemAndSaveToStorage() {
+  allTodoItems.append(buildItem(getInput().value));
   // ES6 spread syntax
-  localTodos = [...localTodos, userInput.value];
-  userInput.value = "";  // Clear the input field
+  localTodos = [...localTodos, getInput().value];
+  saveToStorage();
+}
 
-  // Save data to localStorage
-  localStorage.setItem("todos", JSON.stringify(localTodos));
+addItemBtn.addEventListener('click', () => {
+  if (isValidInput()) addItemAndSaveToStorage();
+  getInput().value = "";  // Clear the input field
 });
